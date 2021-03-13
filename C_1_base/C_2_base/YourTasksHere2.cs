728ad96a04a723e2cabfa5f1984702860ec962f2
Arthur Nersisyan <24681914+Annonymos3108@users.noreply.github.com>
2021-03-13 23:09:31 +0300

    Added Task 2 solution

--- C_1_base/C_2_base/YourTasksHere2.cs
+++ C_1_base/C_2_base/YourTasksHere2.cs
@@ -4,4 +4,78 @@
     // Ко всем фигурам должно быть возможно осуществить доступ по одному имени 
     // Через это имя должно быть возможно получить доступ к площади и периметру фигур
     // Все числа пусть имееют тип double
+
+    abstract class Figure
+    {
+        protected double area;
+        protected double perimeter;
+
+        public double GetArea()
+        {
+            return area;
+        }
+
+        protected virtual void calculate()
+        {
+        }
+    }
+
+    class Reactangle : Figure
+    {
+        private double sideA;
+        private double sideB;
+
+        Reactangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.calculate();
+        }
+
+        protected override void calculate()
+        {
+            base.area = sideA * sideB;
+            base.perimeter = (sideA + sideB) * 2;
+        }
+    }
+
+    class Triangle : Figure
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+            this.calculate();
+        }
+
+        protected override void calculate()
+        {
+            base.perimeter = sideA + sideB + sideC;
+            double halfP = base.perimeter / 2;
+            base.area = System.Math.Sqrt(halfP * (halfP - sideA) * (halfP - sideB) * (halfP - sideC));
+        }
+    }
+
+    class Circle : Figure
+    {
+        private double radius;
+
+        Circle(double radius)
+        {
+            this.radius = radius;
+            this.calculate();
+        }
+
+        protected override void calculate()
+        {
+            base.perimeter = System.Math.PI * 2 * radius;
+            base.area = System.Math.PI * radius * radius;
+        }
+    }
+
 }
